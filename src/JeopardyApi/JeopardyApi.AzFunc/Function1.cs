87754f10469d1116521e8a7ed071a9fe8444800e
Noah Southard <noahsouthard@outlook.com>
2022-11-27 16:17:58 -0700

    Initial Cosmos querying

--- src/JeopardyApi/JeopardyApi.AzFunc/Function1.cs
+++ src/JeopardyApi/JeopardyApi.AzFunc/Function1.cs
@@ -1,5 +1,9 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Net;
+using JeopardyApi.AzFunc.Models;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -10,15 +14,69 @@ namespace JeopardyApi.AzFunc
     {
         private readonly ILogger _logger;
 
+        public string DatabaseName => "Jeopardy";
+        public string QuestionsContainerName => "Questions";
+
         public Function1(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<Function1>();
         }
 
         [Function("Function1")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            using CosmosClient cosmosClient = new(
+                accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
+                authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+            );
+
+            var container = cosmosClient.GetContainer(DatabaseName, QuestionsContainerName);
+            //var containerResult = await db.Database.CreateContainerIfNotExistsAsync(
+            //    id: QuestionsContainerName,
+            //    partitionKeyPath: "/partition")
+            //    .ConfigureAwait(false);
+
+            var question1 = await container.ReadItemAsync<Question>(
+                id: "a962f6d9-6d96-49f4-a260-82215a331030",
+                partitionKey: new PartitionKey(1))
+                .ConfigureAwait(false);
+
+            var firstQuestionsQuery = new QueryDefinition(
+                    query: "SELECT TOP 10 * FROM Questions"
+                );
+
+            using var feedIterator = container.GetItemQueryIterator<Question>(queryDefinition: firstQuestionsQuery);
+
+            var foundQuestions = new List<Question>();
+            while (feedIterator.HasMoreResults)
+            {
+                FeedResponse<Question> feedResponse = await feedIterator.ReadNextAsync();
+                foreach (Question item in feedResponse)
+                {
+                    foundQuestions.Add(item);
+                    Console.WriteLine($"Found item:\t{item.answer}");
+                }
+            }
+
+            var foundQuestions2 = new List<Question>();
+            using (var setIterator = container.GetItemLinqQueryable<Question>()
+                      .Take(10)
+                      .ToFeedIterator())
+            {
+                //Asynchronous query execution
+                while (setIterator.HasMoreResults)
+                {
+                    foreach (var item in await setIterator.ReadNextAsync())
+                    {
+                        {
+                            foundQuestions2.Add(item);
+                            Console.WriteLine(item.answer);
+                        }
+                    }
+                }
+            }
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
---
+++ src/JeopardyApi/JeopardyApi.AzFunc/Models/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeopardyApi.AzFunc.Models
+{
+    public class Question
+    {
+        public int round { get; set; }
+        public int value { get; set; }
+        public string daily_double { get; set; }
+        public string category { get; set; }
+        public string comments { get; set; }
+        public string answer { get; set; }
+        public string question { get; set; }
+        public DateTime air_date { get; set; }
+        public string notes { get; set; }
+        public int partition { get; set; }
+        public string id { get; set; }
+        public string _rid { get; set; }
+        public string _self { get; set; }
+        public string _etag { get; set; }
+        public string _attachments { get; set; }
+        public int _ts { get; set; }
+    }
+
+}
